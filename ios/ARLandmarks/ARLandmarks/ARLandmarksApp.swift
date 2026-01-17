98baacf6f4c4a5db7f47e4079f2f0d367a3dcfc7
JSss10 <jessica.schneiter@bluewin.ch>
2026-01-17 18:46:46 +0100

    feat(ios): create Xcode project with Swift 6 and async-http-client

---
+++ ios/ARLandmarks/ARLandmarks/ARLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARLandmarksApp.swift
+//  ARLandmarks
+//
+//  Created by Jessica Schneiter on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ARLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
